b75cd139dcbc6768a159a2192ba8988676e4d071
Couldi <dudals2014@gmail.com>
2019-01-10 18:04:07 +0900

    please.. please..

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('/', include('blog.urls')),
+    path('', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('portfolio/', include('portfolio.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
